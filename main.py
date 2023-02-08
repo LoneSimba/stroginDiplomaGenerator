55643fb35c204a8cacf3b99e46a35c6a7606dc42
LoneSimba <vovamaster99@gmail.com>
2023-02-08 11:24:56 +0300

    правки + "в ожидании чуда"

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import os
+import sys
+
 import unicodedata
 
 import openpyxl
@@ -15,6 +17,8 @@ from src import MailCloudDownloader
 
 # main() start
 def main():
+    print('Loading config and ingest file\n')
+
     config: dict = load_config()
     comp: str = config.get("comp")
     ingest_path: str = os.path.join(os.getcwd(), "files/", comp)
@@ -25,75 +29,114 @@ def main():
     if table_name is None:
         raise Exception('No table was downloaded')
 
+    print('Load complete, ingest table found: ' + table_name + '\n')
+
     table: Workbook = openpyxl.load_workbook(os.path.join(ingest_path, table_name))
     table_conf: dict = config.get('table').get(comp)
 
-    for i in range(0, table_conf.get('cats') - 1):
+    print('Processing nominations...\n')
+
+    for i in range(0, table_conf.get('cats')):
         ws: Worksheet = table.worksheets[i]
         if not re.findall(r'\d\s', str(ws.title)):
             continue
 
-        nomination: str = re.sub(r'\d\s', '', str(ws.title))
-        start_row: int = 6
+        title: str = re.sub(r'\d\s', '', str(ws.title))
+        print('[' + str(i+1) + '/' + str(table_conf.get('cats')) + '] Processing nomination: ' + title)
+
+        start_row: int = 5
+        processed: int = 0
 
         for row in ws.iter_rows(min_row=start_row, values_only=True):
+            processed += 1
+
             if not row[0]:
                 break
 
             values: list = [row[v] for k, v in table_conf.get('cols').items()]
             entry: dict = dict(zip(table_conf.get('cols').keys(), values))
-            entry.update({'nomination': nomination})
 
-            if 'аннулирован' in entry.get('result'):
-                continue
+            # if any(res in entry.get('result') for res in ['Лауреат III степени']):
+            if True:
 
-            count: int = int(re.findall(r'\d', str(entry.get('count')))[0])
-            is_group: bool = count > 1
-            is_individual: bool = 'Самостоятельный участник' in entry.get('form')
-            is_prized: bool = any(x in entry.get('result') for x in ['Дипломант', 'Лауреат', 'Гран-при'])
+                # if 'Гран-при' in entry.get('result'):
+                #     continue
+                # if 'Специальный' in entry.get('result'):
+                #     continue
+                # if 'аннулирован' in entry.get('result'):
+                #     continue
 
-            output_path: str = os.path.join(
-                os.getcwd(), "output", comp,
-                str((entry.get('tutor'), '')[is_individual]).strip()
-            )
+                if entry.get('count') < 1:
+                    continue
 
-            in_filename: str = "шаблон_"
-            in_filename += ("благодарность", "диплом")[is_prized]
-            in_filename += ("_участник", "_участник_группа")[is_group]
-            in_filename += ("", "_инд")[is_individual]
-            in_filename += ".docx"
+                nomination = entry.get('nomination').replace('"', '')
+                title = re.sub(r'^\"|\"$', '', entry.get("title"))
+                entry.update({'nomination': nomination, "title": title})
 
-            if not is_individual:
-                tutor_ent: dict = {'tutor': entry.get('tutor'), 'group': entry.get('group'),
-                                   'school': entry.get('school')}
-                create_tutor_dipl(tutor_ent, ingest_path, output_path)
+                count: int = int(re.findall(r'\d+', str(entry.get('count')))[0])
+                is_group: bool = count > 1
+                is_individual: bool = 'Самостоятельный участник' in (entry.get('form') or '')
+                is_prized: bool = any(x in entry.get('result') for x in ['Дипломант', 'Лауреат', 'Гран-при', 'Специальный'])
+                is_sp: bool = any(x in entry.get('result') for x in ['Гран-при', 'Специальный'])
+                is_gp: bool = 'Гран-при' in entry.get('result')
 
-            if not is_group:
+                output_path: str = os.path.join(
+                    os.getcwd(), "output", comp, nomination,
+                    str((entry.get('tutor'), '')[is_individual]).strip()
+                )
+
+                in_filename: str = "шаблон_"
+                in_filename += ("благодарность", "диплом")[is_prized]
+                in_filename += ("_участник", "_участник_группа")[is_group and not is_individual]
+                in_filename += ("", "_инд")[is_individual]
+                if is_prized:
+                    in_filename += ("", "_сп")[is_sp]
+
+                in_filename += ".docx"
+
+                if not is_individual:
+                    tutor_ent: dict = {'tutor': entry.get('tutor') or '', 'group': str(entry.get('group')) or '',
+                                       'school': str(entry.get('school')) or ''}
+                    create_tutor_dipl(tutor_ent, ingest_path, output_path, is_gp)
+
+                # if not is_group:
                 create_diploma(entry, os.path.join(ingest_path, in_filename), output_path, is_prized)
-                continue
+                    # continue
 
-            participants = re.sub(r',', ', ', entry.get('participant'))
-            participants = participants.split(',')
+                # participants = re.sub(r',', ', ', str(entry.get('participant')))
+                # participants = participants.split(',')
 
-            if len(participants) != count:
-                create_diploma(entry, os.path.join(ingest_path, in_filename), output_path, is_prized)
-                continue
+                # if len(participants) != count:
+                    # create_diploma(entry, os.path.join(ingest_path, in_filename), output_path, is_prized)
+                    # continue
 
-            for partic in participants:
-                en = dict(entry)
-                en.update({'participant': partic})
-                create_diploma(en, os.path.join(ingest_path, in_filename), output_path, is_prized)
+                # for partic in participants:
+                #     en = dict(entry)
+                #     en.update({'participant': partic})
+                #     create_diploma(en, os.path.join(ingest_path, in_filename), output_path, is_prized)
 
+        print("Nomination done, " + str(processed) + " processed\n")
 
 # main() end
 
 
 def create_diploma(entry: dict, ingest_path: str, output_path: str, is_prized: bool):
+
+    part: str = re.sub(r'[\"\'\\/?%*:|<>\n]', '', str(entry.get('participant')))
+
+    if len(part) > 64:
+        part = part[:64]
+
+    tit: str = re.sub(r'[\"\'\\/?%*:|<>\n]', '', str(entry.get('title')))
+
+    if len(tit) > 64:
+        tit = tit[:64]
+
     out_filename: str = ("благодарность", "диплом")[is_prized]
-    out_filename += re.sub(r'[\"\'\\/?%*:|<>]', '',
-                           (" " + entry.get('participant') + " " + entry.get('title') + ".docx"))
+    out_filename += re.sub(r'[\"\'\\/?%*:|<>\n]', '',  (" " + part + " " + tit))
+
     output_path = unicodedata.normalize('NFKD', output_path)
-    out_filename = unicodedata.normalize('NFKD', os.path.join(output_path, out_filename))
+    out_filename = unicodedata.normalize('NFKD', os.path.join(output_path, out_filename + ".docx"))
 
     temp: Document = D(ingest_path)
     keys: list = list(entry.keys())
@@ -103,23 +146,26 @@ def create_diploma(entry: dict, ingest_path: str, output_path: str, is_prized: b
         for run in par.runs:
             text = str(run.text).replace('%', '')
             if text in keys:
-                run.text = entry.get(text) or ''
+                run.text = str(entry.get(text)) or ''
 
     os.makedirs(output_path, exist_ok=True)
     temp.save(out_filename)
 
 
-def create_tutor_dipl(entry: dict, ingest_path: str, output_path: str):
+def create_tutor_dipl(entry: dict, ingest_path: str, output_path: str, is_gp: bool):
     out_filename: str = "благодарность"
-    out_filename += re.sub(r'["\'\\/?%*:|<>]', '',
-                           (" " + entry.get('tutor') + " " + entry.get('group') + " " + entry.get('school') + ".docx"))
+    out_filename += re.sub(r'["\'\\/?%*:|<>\n]', '',
+                           (" " + str(entry.get('tutor')) + " " + entry.get('group') + " " + entry.get('school') + ".docx"))
     output_path = unicodedata.normalize('NFKD', output_path)
     out_filename = unicodedata.normalize('NFKD', os.path.join(output_path, out_filename))
 
     if os.path.isfile(out_filename):
         return
 
-    temp: Document = D(os.path.join(ingest_path, "шаблон_благодарность_педагог.docx"))
+    temp_name: str = "шаблон_благодарность_педагог"
+    temp_name += ("", "_гп")[is_gp]
+    temp_name += ".docx"
+    temp: Document = D(os.path.join(ingest_path, temp_name))
     keys: list = list(entry.keys())
     table: Table = temp.tables[0]
     for c in table.column_cells(0):
@@ -153,5 +199,10 @@ def load_config() -> dict:
 if __name__ == '__main__':
     try:
         main()
+        print("Done")
     except Exception as e:
-        print("Fatal error: " + str(e))
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        f = exc_tb.tb_frame
+        file = f.f_code.co_filename
+
+        print("Fatal error: " + str(e) + " in " + file + " at " + str(exc_tb.tb_lineno))
